38c5866d97e1f8e38ba88c30f57f5b7cea01f111
adasilvapdev <adasilvapdev@gmail.com>
2021-07-02 19:41:19 -0400

    Added functionality to home views

--- app/screens/home/HomeScreen.tsx
+++ app/screens/home/HomeScreen.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+    const navigation = useNavigation()
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Home Screen</Text>
+            <Button
+                title="Sub Screen"
+                onPress={() => navigation.navigate('HomeSubScreen')}
+            />
         </View>
     )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignSelf: "center",
+    }
+})
--- app/screens/home/HomeSubScreen.tsx
+++ app/screens/home/HomeSubScreen.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
 
 const HomeSubScreen = () => {
+    const navigation = useNavigation()
+
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Home Sub Screen</Text>
+            <Button
+                title="Go back"
+                color="purple"
+                onPress={() => navigation.goBack()}
+            />
         </View>
     )
 }
 
 export default HomeSubScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignSelf: "center",
+    }
+})
+
